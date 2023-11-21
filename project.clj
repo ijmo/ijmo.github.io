f3955c02c963399c88af842888dd462c0296916e
Jinmo Im <jinmo.im@gmail.com>
2023-11-22 00:25:20 +0900

    Update cryogen

--- project.clj
+++ project.clj
@@ -3,13 +3,15 @@
             :url "https://github.com/lacarmen/cryogen"
             :license {:name "Eclipse Public License"
                       :url "http://www.eclipse.org/legal/epl-v10.html"}
-            :dependencies [[org.clojure/clojure "1.10.0"]
-                           [ring/ring-devel "1.7.1"]
-                           [compojure "1.6.1"]
+            :dependencies [[org.clojure/clojure "1.11.1"]
+                           [ring/ring-devel "1.8.2"]
+                           [compojure "1.6.2"]
                            [ring-server "0.5.0"]
-                           [cryogen-asciidoc "0.1.4"]
-                           [cryogen-core "0.2.1"]]
+                           [cryogen-asciidoc "0.3.4"]
+                           [cryogen-core "0.4.4"]]
             :plugins [[lein-ring "0.12.5"]]
-            :main cryogen.core
-            :ring {:init cryogen.server/init
-                   :handler cryogen.server/handler})
+                      :main cryogen.core
+                      :ring {:init cryogen.server/init
+                             :handler cryogen.server/handler}
+                      :aliases {"serve"      ["run" "-m" "cryogen.server"]
+                                "serve:fast" ["run" "-m" "cryogen.server" "fast"]})
